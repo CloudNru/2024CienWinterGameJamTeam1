5982ac13d4a7d080755af9d060edef0f5623cc01
heehoooo <hehanho74@gmail.com>
2024-02-05 23:01:07 +0900

    add a lot of assets

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -4,30 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
+    public GameObject howToPlayPanel;
     public void GameStart()
     {
         SceneManager.LoadScene("MakingUIScene");
     }
     public void ShowHowToPlay()
     {
-        
+        howToPlayPanel.SetActive(true);
     }
     public void ExitGame()
     {
         Debug.Log("Exit!");
         Application.Quit();
+    }
+    public void ClosePanel()
+    {
+        howToPlayPanel.SetActive(false);
     }
 
 
